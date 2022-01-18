c6fbf9e1489458c8d8408b998bcff74ed574222b
ktaehyung7 <55673916+ktaehyung7@users.noreply.github.com>
2022-01-18 14:05:39 +0900

    Add files via upload

--- canPlaceFlowers.py
+++ canPlaceFlowers.py
@@ -3,13 +3,13 @@ class Solution:
         t = 0
         for k in range(len(flowerbed)):
             if k==0:
-                if flowerbed[k] == 0 and flowerbed[k+1] ==0: t +=1
+                if flowerbed[k] + flowerbed[k+1] == 0: t +=1
             elif k==len(flowerbed)-1:
-                if flowerbed[k-1] == 0 and flowerbed[k] == 0: t +=1
+                if flowerbed[k-1] + flowerbed[k] == 0: t +=1
             else:
-                if flowerbed[k-1] ==0 and flowerbed[k]==0 and flowerbed[k+1]==0: t +=1
+                if flowerbed[k-1] + flowerbed[k] + flowerbed[k+1]==0: t +=1
             
         return t >= n
     
 a = Solution()
-aa = a.canPlaceFlowers([1,0,0,0,1], 2)
+aa = a.canPlaceFlowers([1,0,0,0,0,0,1], 3)
