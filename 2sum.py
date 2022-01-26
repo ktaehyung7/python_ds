32faafbea455a205275a184fcb94c696dc15a7c1
ktaehyung7 <55673916+ktaehyung7@users.noreply.github.com>
2022-01-27 08:58:18 +0900

    Update 2sum.py

--- 2sum.py
+++ 2sum.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
         hashmap = {}
              
         for i in range(len(nums)):
